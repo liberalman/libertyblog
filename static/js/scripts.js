142ad8f07cf9e84393774975e835666be8d7f4d3
liberalman <zscchina@163.com>
2017-07-21 16:11:59 +0800

    register error

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -127,49 +127,4 @@
 
     // Form validation - sign up
 	toastr.options = {"positionClass": "toast-top-full-width"};
-    if( $('.form-register').length ) {
-      $('.form-register').validate({
-        rules: {
-          password: {
-            minlength: 5
-          },
-          confirmPassword: {
-            minlength: 5,
-            equalTo: "#password"
-          }
-        },
-        messages: {
-          fullname: "Please enter your fullname",
-          password: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long"
-          },
-          confirmPassword: {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 5 characters long",
-            equalTo: "Please enter the same password as above"
-          },
-          email: "Please enter a valid email address"
-        },      
-        submitHandler: function(form) {
-          var $this = $(form);
-          $.ajax({
-            url: $this.attr('action'),
-            type: 'POST',
-            data: $this.serialize(),
-          })
-          .done(function(msg) {
-            if( msg == 'ok' ) {
-              toastr.success('Thank you for signing up.');
-              $this[0].reset();
-            } else {
-              toastr.error('An error occured. Please try again later.');
-            }
-          })
-          .fail(function() {
-            toastr.error('An error occured. Please try again later.');
-          });
-        }      
-      });
-    }
 })();
