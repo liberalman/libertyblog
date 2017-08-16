5154a7abe3f0a48f732324f5c184d0c0f3ab272a
liberalman <zscchina@163.com>
2017-08-16 21:33:36 +0800

    1

--- controllers/blog/base.go
+++ controllers/blog/base.go
@@ -210,7 +210,7 @@ func (this *baseController) isPost() bool {
 
 //获取用户IP地址
 func (this *baseController) getClientIp() string {
-	ip := this.Ctx.Request.Header.Get("x-forwarded-for")
+	/*ip := this.Ctx.Request.Header.Get("x-forwarded-for")
 	if 0 == len(ip) || "unknown" == ip {
 		ip = this.Ctx.Request.Header.Get("proxy-client-ip")
 	}
@@ -220,7 +220,8 @@ func (this *baseController) getClientIp() string {
 	if 0 == len(ip) || "unknown" == ip {
 		//ip = (strings.Split(this.Ctx.Request.RemoteAddr, ":"))[0]
 		ip = this.Ctx.Request.RemoteAddr
-	}
+	}*/
+	ip := (strings.Split(this.Ctx.Request.RemoteAddr, ":"))[0]
 	return ip
 }
 
