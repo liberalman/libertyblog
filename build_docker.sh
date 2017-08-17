b2618218b720683df72e91a4ebf4c8a02a67f901
liberalman <zscchina@163.com>
2017-08-17 14:21:16 +0800

    build

---
+++ build_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+git pull
+
+docker-compose down
+
+docker rm `docker ps -a -q` -f
+
+docker rmi $(docker images -f "dangling=true" -q) -f
+
+docker run -it -v /data/gocode:/data/gocode -e GOPATH=/data/gocode --rm demo/go-build:latest /bin/bash -c "cd /data/gocode/src/libertyblog && go build"
+
+docker build -t liberalman/libertyblog .
+
+docker-compose up -d
+
