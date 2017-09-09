ba8c55b4b1086e6aa8d0120e1616de276b5e4b79
liberalman <zscchina@163.com>
2017-09-09 17:18:25 +0800

    1

--- controllers/blog/upyun.go
+++ controllers/blog/upyun.go
@@ -33,22 +33,28 @@ func (this *UpyunController) SignaturPolicy() {
 	//t := time.Now().UnixNano()
 	//filetype := "jpg"
 	//filename = fmt.Sprintf("%s/%d.%s", savepath, t, filetype)
+
+	/*t, _ := this.GetInt64("date")
+	now := time.Unix(t, 0)*/
+
 	now := time.Now()
-	format := "Mon, 02 Jan 2006 15:04:05 GMT" // 必须的这个格式，要和又拍云的时间格式完全一致
-	var date string = now.Format(format)
-	var content_md5 string = this.GetString("content_md5") // "7ac66c0f148de9519b8bd264312c4d64"
-	expiration := now.Unix() + 300                         // 授权过期时间，设为当前时间往后延5分钟
+	var date string = now.Format("Mon, 02 Jan 2006 15:04:05 GMT") // 必须的这个格式，要和又拍云的时间格式完全一致
+	var content_md5 string = this.GetString("content_md5")        // "7ac66c0f148de9519b8bd264312c4d64"
+	expiration := now.Unix() + 300                                // 授权过期时间，设为当前时间往后延5分钟.自定义签名有效期，建议为 30 分钟。如果签名超过有效期，需要重新生成签名。
 
 	var tosignature string
-	var topolicy string
+	var policy string
 	if len(content_md5) > 0 {
-		tosignature = method + "&" + uri + "&" + date + "&" + content_md5
-		topolicy = fmt.Sprintf("{\"bucket\": \"%s\", \"save-key\": \"%s\", \"expiration\": \"%d\", \"date\": \"Wed, 09 Nov 2016 14:26:58 GMT\", \"content-md5\": \"%s\"}",
-			upyun_bucket, uri, expiration, content_md5) // expiration:1478674618
+		tmp := fmt.Sprintf("{\"bucket\": \"%s\", \"save-key\": \"%s\", \"expiration\": \"%d\", \"date\": \"%s\", \"content-md5\": \"%s\"}",
+			upyun_bucket, uri, expiration, date, content_md5)
+		policy = base64.StdEncoding.EncodeToString([]byte(tmp))
+		tosignature = method + "&" + uri + "&" + date + "&" + policy + "&" + content_md5
 	} else {
-		tosignature = method + "&" + uri + "&" + date
-		topolicy = fmt.Sprintf("{\"bucket\": \"%s\", \"save-key\": \"%s\", \"expiration\": \"%d\", \"date\": \"Wed, 09 Nov 2016 14:26:58 GMT\"}",
-			upyun_bucket, uri, expiration) // expiration:1478674618
+
+		tmp := fmt.Sprintf("{\"bucket\": \"%s\", \"save-key\": \"%s\", \"expiration\": \"%d\", \"date\": \"%s\"}",
+			upyun_bucket, uri, expiration, date)
+		policy = base64.StdEncoding.EncodeToString([]byte(tmp))
+		tosignature = method + "&" + uri + "&" + date + "&" + policy
 	}
 
 	//hmac ,use sha1
@@ -57,13 +63,13 @@ func (this *UpyunController) SignaturPolicy() {
 	mac.Write([]byte(tosignature))
 	//Go 同时支持标准的和 URL 兼容的 base64 格式。编码需要使用 []byte 类型的参数，所以要将字符串转成此类型。
 	signature := base64.StdEncoding.EncodeToString(mac.Sum(nil))
-	policy := base64.StdEncoding.EncodeToString([]byte(topolicy))
 	authorization := fmt.Sprintf("UPYUN %s:%s", upyun_operator, signature)
 
 	ret := models.Ret{Code: 0, Message: "get signature success"}
 	var data map[string]string = map[string]string{}
 	data["signature"] = signature
 	data["policy"] = policy
+	data["date"] = date
 	data["authorization"] = authorization
 	ret.Data = data
 	this.Data["json"] = ret
