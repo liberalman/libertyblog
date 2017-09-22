c4de66555ff15afbfb44d65c746324254b6675d0
liberalman <zscchina@163.com>
2017-09-22 19:24:01 +0800

    1

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+function check_env()
+{
 if [ ! -d "/var/coreseek/" ]; then
     mkdir /var/coreseek/
 fi
@@ -10,17 +12,67 @@ if [ ! -d "/var/coreseek/data" ]; then
     mkdir /var/coreseek/data
 fi
 
-git pull
+    git pull
+}
 
-docker-compose down
 
-docker rm `docker ps -a -q` -f
+function clean()
+{
+    docker rmi $(docker images -f "dangling=true" -q) -f
+}
 
-docker rmi $(docker images -f "dangling=true" -q) -f
+function Usage()
+{
+    echo "Usage: $0 build|recreate|clean"
+    exit -1
+}
 
-docker run -it -v /data/gocode:/data/gocode -e GOPATH=/data/gocode --rm demo/go-build:latest /bin/bash -c "cd /data/gocode/src/libertyblog && go build"
+function check_services()
+{
+    arr=("loadbalancer database registrator consul-server registrator sphinx")
+    for service in ${arr[@]};do
+        echo $service
+        exist=`docker-compose ps|grep $service|grep Up`
+        if [ "" == "$exist" ]; then
+            # not exist
+            docker-compose start $service
+        fi
+    done
+}
 
-docker build -t liberalman/libertyblog .
+function build()
+{
+    docker-compose stop web
+    docker run -it -v /data/gocode:/data/gocode -e GOPATH=/data/gocode --rm demo/go-build:latest /bin/bash -c "cd /data/gocode/src/libertyblog && go build"
+    docker build -t liberalman/libertyblog .
+    docker-compose star web
+}
 
-docker-compose up -d
+function recreate()
+{
+    docker-compose down
+    docker rm `docker ps -a -q` -f
+    docker-compose up -d
+    exit 0
+}
+
+if [ $# -ne 0 ]; then
+case $1 in
+    "build")
+        build;
+        ;;
+    "recreate")
+        recreate;
+        ;;
+    "clean")
+        clean;
+        ;;
+    *)
+        Usage
+esac
+fi
+
+clean
+check_env
+check_services
 
