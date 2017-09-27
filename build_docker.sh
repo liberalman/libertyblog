d3de09704a895464ed3dfcda6cf9d03fef5e6366
liberalman <zscchina@163.com>
2017-09-27 17:13:57 +0800

    1

--- build_docker.sh
+++ build_docker.sh
@@ -2,16 +2,15 @@
 
 function check_env()
 {
-if [ ! -d "/var/coreseek/" ]; then
-    mkdir /var/coreseek/
-fi
-if [ ! -d "/var/coreseek/log" ]; then
-    mkdir /var/coreseek/log
-fi
-if [ ! -d "/var/coreseek/data" ]; then
-    mkdir /var/coreseek/data
-fi
-
+    if [ ! -d "/var/coreseek/" ]; then
+        mkdir /var/coreseek/
+    fi
+    if [ ! -d "/var/coreseek/log" ]; then
+        mkdir /var/coreseek/log
+    fi
+    if [ ! -d "/var/coreseek/data" ]; then
+        mkdir /var/coreseek/data
+    fi
 }
 
 
@@ -43,9 +42,12 @@ function build()
 {
     git pull
     docker-compose stop web
+    docker-compose rm web
     docker run -it -v /data/gocode:/data/gocode -e GOPATH=/data/gocode --rm demo/go-build:latest /bin/bash -c "cd /data/gocode/src/libertyblog && go build"
     docker build -t liberalman/libertyblog .
-    docker-compose star web
+    #docker-compose build web
+    #docker-compose star web
+    docker-compose up --no-recreate -d
 }
 
 function recreate()
